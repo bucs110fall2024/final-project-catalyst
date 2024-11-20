b1ed05291d4920804008140366b1cac26bc69f15
tristan <tburchard@binghamton.edu>
2024-11-20 18:24:44 -0500

    Classes

---
+++ src/deck.py
@@ -0,0 +1,4 @@
+class Deck:
+    pass
+
+#This class will take care of shuffling the deck, dealing cards to the player/CPU, and providing hits.
---
+++ src/hand.py
@@ -0,0 +1,4 @@
+class Hand:
+    pass
+
+#This class will take care of calculating the numbers (how much the player has in comparison to the CPU and who is currently winning/not)
